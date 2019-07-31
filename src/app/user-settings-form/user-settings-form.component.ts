d9fd12341df62472a1029141d3c5b0d359dc955b
Nick Thacker <NThacker@equitable.ca>
2019-07-31 15:35:50 -0400

    Added user settings model and included two-way binding with a copied version of user settings to eliminate data corruption

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserSettings } from './../data/user-settings';
+
 @Component({
   selector: 'app-user-settings-form',
   templateUrl: './user-settings-form.component.html',
   styleUrls: ['./user-settings-form.component.css']
 })
 export class UserSettingsFormComponent implements OnInit {
+
+  originalUserSettings: UserSettings = {
+    name: 'Nick',
+    emailOffers: true,
+    interfaceStyle: 'dark',
+    subscriptionType: 'Annual',
+    notes: 'Here are some notes...'
+  };
+
+  userSettings: UserSettings = { ...this.originalUserSettings };
 
   constructor() { }
 
